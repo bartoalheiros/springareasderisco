90d89db80c45f797fdbffe6ce6a06201234333d8
BartoAlheiros <alheirosb@gmail.com>
2023-09-03 11:55:19 -0300

    adicionando global exception handler

--- src/main/java/com/br/ufrpe/devweb/tsistema/exception/CallNotFoundException.java
+++ src/main/java/com/br/ufrpe/devweb/tsistema/exception/CallNotFoundException.java
@@ -3,7 +3,7 @@ package com.br.ufrpe.devweb.tsistema.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such Chamado")  // 404
+@ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such Call")  // 404
 public class CallNotFoundException extends  RuntimeException {
   
   private static final long serialVersionUID = 137744895179504666L;
---
+++ src/main/java/com/br/ufrpe/devweb/tsistema/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.br.ufrpe.devweb.tsistema.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CallNotFoundException.class)
+    public ResponseEntity<String> handleCallNotFoundException(CallNotFoundException ex) {
+        return new ResponseEntity<>("Call not found: " + ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    // Outros métodos @ExceptionHandler para tratar exceções específicas
+}
