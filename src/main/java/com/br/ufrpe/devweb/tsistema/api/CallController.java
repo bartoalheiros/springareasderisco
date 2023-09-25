f13067b9a14205f0e3a803de0f85d05d6841a77b
BartoAlheiros <alheirosb@gmail.com>
2023-09-25 10:04:23 -0300

    fix: callcontroller organize imports

--- src/main/java/com/br/ufrpe/devweb/tsistema/api/CallController.java
+++ src/main/java/com/br/ufrpe/devweb/tsistema/api/CallController.java
@@ -1,7 +1,6 @@
 package com.br.ufrpe.devweb.tsistema.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.br.ufrpe.devweb.tsistema.exception.CallNotFoundException;
 import com.br.ufrpe.devweb.tsistema.model.entities.Call;
 import com.br.ufrpe.devweb.tsistema.service.CallService;
 
